2735a1eaaf118b5679531d4d9ea8ad1bcee9ad42
Muhammad Munir <mmunir1214@gmail.com>
2023-10-07 19:26:57 -0400

    Fixing more issues

--- openAI-Backend/gptIndex.js
+++ openAI-Backend/gptIndex.js
@@ -3,7 +3,7 @@ Node.js is used to run the server
 Express.js is used to define routes, handle HTTP requests, and set up middleware for parsing request bodies and enabling CORS.
 */
 
-require("dotenv").config({ path: ".env.gpt" });
+require("dotenv").config({ path: "./.env.gpt" });
 const cors = require("cors");
 const OpenAI = require("openai");
 const express = require("express");
