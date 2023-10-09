e38ae6677943d572e596df80e5fae9e974d9cd21
Muhammad Munir <mmunir1214@gmail.com>
2023-10-09 18:24:45 -0400

    Frontend ready for deployment

--- src/index.js
+++ src/index.js
@@ -34,60 +34,8 @@ ReactDOM.render(
         />
         <Route path="register" element={<Signup />} />
         <Route path="login" element={<Login />} />
-        {/* <Route path="home" element={<Home />} /> */}
       </Routes>
     </AuthProvider>
   </Router>,
   document.getElementById("root")
 );
-
-// import React from "react";
-// import ReactDOM from "react-dom";
-// import App from "./App";
-// import "./index.css";
-// import "./App.css";
-// import Pdf from "./containers/pdf/Pdf";
-// import OpenAI from "./containers/openAI/OpenAI";
-// import Signup from "./register-backend/authentication/Signup";
-// import Login from "./register-backend/authentication/Login";
-// import PrivateRoute from "./register-backend/authentication/PrivateRoute";
-// import {
-//   createBrowserRouter,
-//   RouterProvider,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
-
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <App />,
-//   },
-//   {
-//     path: "pdf",
-//     element: <PrivateRoute component={Pdf} />,
-//   },
-//   {
-//     path: "openAI",
-//     element: <PrivateRoute component={OpenAI} />,
-//   },
-//   {
-//     path: "home",
-//     element: <home />,
-//   },
-//   {
-//     path: "register",
-//     element: <Signup />,
-//   },
-//   {
-//     path: "login",
-//     element: <Login />,
-//   },
-// ]);
-
-// ReactDOM.render(
-//   <RouterProvider router={router}>
-//     <Routes>{router}</Routes>
-//   </RouterProvider>,
-//   document.getElementById("root")
-// );
